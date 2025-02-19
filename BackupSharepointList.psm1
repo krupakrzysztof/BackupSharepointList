5e805adfd8503338ef6e027e955bd8ff82cf45e9
Krzysztof Krupa <87368964+krupakrzysztof@users.noreply.github.com>
2025-02-19 18:10:46 +0100

    Update BackupSharepointList.psm1

--- BackupSharepointList.psm1
+++ BackupSharepointList.psm1
@@ -8,7 +8,7 @@
 
     $items = Get-PnPListItem -List $ListName -Connection $Connection
 
-    $standardColumns = @( "ContentTypeId", "Modified", "Created", "_ModerationComments", "File_x0020_Type", "_ColorHex", "_ColorTag", "_Emoji", "ComplianceAssetId", "ID", "_HasCopyDestinations", "_CopySource", "owshiddenversion", "WorkflowVersion", "_UIVersion", "_UIVersionString", "Attachments", "_ModerationStatus", "InstanceID", "Order", "WorkflowInstanceID", "FileRef", "FileDirRef", "Last_x0020_Modified", "Created_x0020_Date", "FSObjType", "SortBehavior", "FileLeafRef", "UniqueId", "ParentUniqueId", "SyncClientId", "ProgId", "ScopeId", "MetaInfo", "_Level", "_IsCurrentVersion", "ItemChildCount", "FolderChildCount", "Restricted", "OriginatorId", "NoExecute", "ContentVersion", "_ComplianceFlags", "_ComplianceTag", "_ComplianceTagWrittenTime", "_ComplianceTagUserId", "AccessPolicy", "_VirusStatus", "_VirusVendorID", "_VirusInfo", "_RansomwareAnomalyMetaInfo", "AppAuthor", "AppEditor", "SMTotalSize", "SMLastModifiedDate", "SMTotalFileStreamSize", "SMTotalFileCount", "_CommentFlags", "_CommentCount", "_DraftOwnerId" )
+    $standardColumns = @( "ContentTypeId", "Modified", "Created", "_ModerationComments", "File_x0020_Type", "_ColorHex", "_ColorTag", "_Emoji", "ComplianceAssetId", "_HasCopyDestinations", "_CopySource", "owshiddenversion", "WorkflowVersion", "_UIVersion", "_UIVersionString", "Attachments", "_ModerationStatus", "InstanceID", "Order", "WorkflowInstanceID", "FileRef", "FileDirRef", "Last_x0020_Modified", "Created_x0020_Date", "FSObjType", "SortBehavior", "FileLeafRef", "UniqueId", "ParentUniqueId", "SyncClientId", "ProgId", "ScopeId", "MetaInfo", "_Level", "_IsCurrentVersion", "ItemChildCount", "FolderChildCount", "Restricted", "OriginatorId", "NoExecute", "ContentVersion", "_ComplianceFlags", "_ComplianceTag", "_ComplianceTagWrittenTime", "_ComplianceTagUserId", "AccessPolicy", "_VirusStatus", "_VirusVendorID", "_VirusInfo", "_RansomwareAnomalyMetaInfo", "AppAuthor", "AppEditor", "SMTotalSize", "SMLastModifiedDate", "SMTotalFileStreamSize", "SMTotalFileCount", "_CommentFlags", "_CommentCount", "_DraftOwnerId" )
 
     $objects = @()
 
@@ -64,19 +64,50 @@ function Restore-SharepointListItems {
     param (
         [PnP.PowerShell.Commands.Base.PnPConnection]$Connection,
         [string]$ListName,
-        [string]$Path
+        [string]$Path,
+        [switch]$TryRestoreIds
     )
 
-    $objects = Get-Content -Path $Path | ConvertFrom-Json
+    $objects = Get-Content -Path $Path | ConvertFrom-Json | Sort-Object -Property ID
 
-    foreach ($object in $objects)
+    $restoreIdsPossible = $false
+    for ($i = 0; $i -lt $objects.Count; $i++)
     {
+        $object = $objects[$i]
+        $status = "Title=$($object.Title)"
+        if ($TryRestoreIds.IsPresent)
+        {
+            $status += "; ID=$($object.ID)"
+        }
+        Write-Progress -Activity "$i / $($objects.Count)" -Status $status -PercentComplete ($i / $objects.Count * 100)
         $values = @{ }
-        foreach ($property in ($object | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name))
+        foreach ($property in ($object | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object { $_ -ne "ID" }))
         {
             $values.Add($property, $object.$property)
         }
-        Add-PnPListItem -List $ListName -Values $values -Connection $Connection
+        do
+        {
+            $newItem = Add-PnPListItem -List $ListName -Values $values -Connection $Connection
+            if ($TryRestoreIds.IsPresent)
+            {
+                if ($i -eq 0 -and $newItem.Id -le $object.ID)
+                {
+                    $restoreIdsPossible = $true
+                }
+                if ((-not $restoreIdsPossible) -or ($newItem.Id -eq $object.ID))
+                {
+                    break
+                }
+                if ($newItem.Id -lt $object.ID)
+                {
+                    Remove-PnPListItem -List $ListName -Identity $newItem.Id -Force -Connection $Connection
+                }
+            }
+            else
+            {
+                break
+            }
+        } while ($true)
     }
 }
 
